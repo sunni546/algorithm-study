1654ff1e57c0128ad445060315d603f9c2630471
Sunhee Kim <sunni372@icloud.com>
2024-01-23 19:10:30 +0900

    programmers: Day 11

---
+++ programmers/입문 문제/팩토리얼.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int i = 1;
+        while(i <= n) {
+            answer++;
+            i *= answer;
+            if (i > n) {
+                answer--;
+            }
+        }
+        
+        return answer;
+    }
+}
