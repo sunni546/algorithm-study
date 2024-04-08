8afd4a9408ecef512e15479e9a3b0d3950300d79
SunHee <76764643+sunni546@users.noreply.github.com>
2024-04-09 00:07:59 +0900

    23971. ZOAC 4
    O

---
+++ baekjoon/python/23971.py
@@ -0,0 +1,8 @@
+def cal(x, y):
+    return 1 + (x - 1) // (y + 1)
+    # (x + y) // (y + 1) 도 가능
+
+
+if __name__ == '__main__':
+    h, w, n, m = map(int, input().split())
+    print(cal(h, n) * cal(w, m))
