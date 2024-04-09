cd12f7a208cd6ca45604f707c7f0c6caf57e6012
SunHee <76764643+sunni546@users.noreply.github.com>
2024-04-09 18:13:59 +0900

    5073. 삼각형과 세 변

---
+++ baekjoon/python/5073.py
@@ -0,0 +1,27 @@
+def is_valid(t):
+    if t[0] < t[1] + t[2]:
+        return True
+    return False
+
+
+def print_type(t):
+    if t[0] == t[1] == t[2]:
+        print("Equilateral")
+    elif t[0] != t[1] != t[2]:
+        print("Scalene")
+    else:
+        print("Isosceles")
+
+
+if __name__ == '__main__':
+    while True:
+        triangle = list(map(int, input().split()))
+        triangle.sort(reverse=True)
+
+        if triangle == [0, 0, 0]:
+            break
+        else:
+            if not is_valid(triangle):
+                print("Invalid")
+            else:
+                print_type(triangle)
