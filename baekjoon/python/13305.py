bd2cac4aa5346b1a523315ca86d1be36b78e0808
SunHee <76764643+sunni546@users.noreply.github.com>
2024-04-25 22:38:54 +0900

    (수정) 13305. 주유소
    min() & sum() 사용 불필요

--- baekjoon/python/13305.py
+++ baekjoon/python/13305.py
@@ -5,20 +5,16 @@ if __name__ == '__main__':
     n = int(input())
     roads = list(map(int, input().split()))
     oil_prices = list(map(int, input().split()))
-    oil_prices.pop()
 
-    min_price = min(oil_prices)
-    min_index = oil_prices.index(min_price)
+    totals = 0
     
-    totals = min_price * sum(roads[min_index:])
+    oil_price = oil_prices[0]
+    
+    for i in range(n - 1):
+        if oil_prices[i] < oil_price:
+            oil_price = oil_prices[i]
 
-    if min_index != 0:
-        oil_price = oil_prices[0]
-
-        for i in range(min_index):
-            if oil_prices[i] < oil_price:
-                oil_price = oil_prices[i]
-
-            totals += oil_price * roads[i]
+        totals += oil_price * roads[i]
 
     print(totals)
+    
